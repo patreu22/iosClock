ff83c985006443c0a251eda958e26f5046474bb9
patreu22 <patrick.reuter@campus.tu-berlin.de>
2015-09-02 01:13:19 +0200

    Running clock. Possible to start, pause and restart

--- EggClock/ViewController.swift
+++ EggClock/ViewController.swift
@@ -9,10 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var time = 0.00
+    var timer = NSTimer()
+    var timerActive = false
+    
+    @IBOutlet var clock: UILabel!
+    @IBAction func startClock(sender: AnyObject) {
+       startTimer()
+    }
+    
+    
+    @IBAction func restart(sender: AnyObject) {
+        timer.invalidate()
+        time = -1.00
+        timerActive = false
+        startTimer()
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+        timerActive = false
+    }
+    
+    
+    func timerResult(){
+        time++
+        displayNewTime()
+    }
+    
+    func displayNewTime(){
+        let seconds = Int(time % 60.0)
+        let minutes = Int(floor(time/60.0))
+        var displayString = ""
+        if minutes<=9{
+           displayString += "0\(minutes)"
+        } else{
+            displayString+="\(minutes)"
+        }
+        
+        displayString+=":"
+        
+        if seconds<=9{
+           displayString += "0\(seconds)"
+        }else{
+           displayString += "\(seconds)"
+        }
+        clock.text = displayString
+    }
+    
+    func startTimer(){
+        if !timerActive{
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("timerResult"), userInfo: nil, repeats: true)
+        timerActive = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
